e7c9ff7ddd91029f3b266ece65f1cd0fdb8644d6
melievkh <khushnudmeliev@gmail.com>
2026-02-06 12:22:24 +0500

    changes

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-operator = input("enter an operator (+,-,*,/): ")
+def name(string):
+    return string
 
-num1 = int(input("enter the first number: "))
-num2 = int(input("enter the second number: "))
 
-if operator == "+":
-    print((num1 + num2))
-elif operator == "-":
-    print(round(num1 - num2))
-elif operator == "*":
-    print(f"{num1} multipled by {num2} is {round(num1*num2)}")
-else:
-    print(round(num1 / num2))
+print(name("husan"))
