aaa672acedeb6a251747e18ff944482f6bcc3775
melievkh <khushnudmeliev@gmail.com>
2026-02-10 17:33:07 +0500

    romantoint

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-for x in [1, 2, 3]:
-    for y in [1, 2]:
-        print(x, y)
+def romanToInt(s):
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    res = 0
+    for i in range(len(s)):
+        if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+            res = res - roman[s[i]]
+        else:
+            res = res + roman[s[i]]
+    return res
+
+
+print(romanToInt("II"))
